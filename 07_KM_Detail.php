7abc2d20ebb18addf2bcd2e01b4e16da2eb3f3f2
Sarun Seepun <sarun_sla@hotmail.com>
2020-12-14 13:38:34 -0800

    Complete 07_KM_Detail

--- 07_KM_Detail.php
+++ 07_KM_Detail.php
@@ -7,7 +7,182 @@
 <body class="loading">
     <?php include_once('include/topnav.php'); ?>
 
-    <img class="img" src="public/assets/app/images/page/07_KM_Detail.jpg" alt="Page" />
+    <?php
+        $breadcrumb = [
+            [ 'url' => '#', 'display' => 'คลังความรู้' ],
+            [ 'url' => '#', 'display' => 'บทความ' ],
+        ];
+        include('component/breadcrumb.php');
+    ?>
+
+    <section class="section-bpadding">
+        <div class="container">
+            <div class="grids">
+                <div class="grid xl-20 lg-25 sm-100"></div>
+                <div class="grid xl-80 lg-75 sm-100 mt-0">
+                    <h3 class="text-center sm xs-no-br" data-aos="fade-up" data-aos-delay="0">
+                        ดึงเอกชนร่วมพีพีพีจัดระเบียบสายสื่อสารลงดิน 
+                        <br> วางระบบ 5G ในนิคมฯ
+                    </h3>
+                </div>
+            </div>
+
+            <div class="tab-container tab-container-01">
+                <div class="grids">
+
+                    <div class="grid xl-20 lg-25 sm-100" data-aos="fade-up" data-aos-delay="150">
+                        <div class="tabs">
+                            <div class="ss-box-shadow"></div>
+                            <a class="tab tab-main color-black h-color-04 active" href="#">
+                                <div class="icon">
+                                    <img src="public/assets/app/images/icon/tab-1.png" alt="Icon">
+                                </div>
+                                <h6>บทความ</h6>
+                            </a>
+                            <a class="tab tab-main color-black h-color-04" href="#">
+                                <div class="icon">
+                                    <img src="public/assets/app/images/icon/tab-8.png" alt="Icon">
+                                </div>
+                                <h6>วิดีทัศน์ / E-learning</h6>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="grid xl-80 lg-75 sm-100">
+                        <div class="content-banner" data-aos="fade-up" data-aos-delay="0">
+                            <div class="ss-img">
+                                <div class="img-bg lazy-bg" data-src="public/assets/app/images/bg/17.jpg"></div>
+                            </div>
+                            <p class="text-right mt-2">
+                                <i class="zmdi zmdi-eye mr-1"></i> 
+                                ยอดการเปิดอ่าน <span class="fw-500">126485</span> ครั้ง
+                            </p>
+                        </div>
+
+                        <div data-aos="fade-up" data-aos-delay="0">
+                            <h3 class="xs fw-200 mt-4 pt-1" style="max-width:44rem;">
+                                บอร์ดกนอ.ไฟเขียวลงทุนนำสายสื่อสารในนิคมอุตสาหกรรมลงใต้ดิน 
+                                <span class="fw-600 color-06">
+                                    รองรับ Smart City เตรียมวางระบบ 5 G
+                                </span>
+                                ครอบคลุมพื้นที่นิคมอุตสาหกรรรม 14 แห่ง 
+                                และท่าเรืออุตสาหกรรม 1 แห่ง คาดเริ่มได้ปี’67
+                            </h3>
+                            <p class="mt-3">
+                                น.ส.สมจิณณ์ พิลึก ผู้ว่าการการนิคมอุตสาหกรรมแห่งประเทศไทย (กนอ.) เปิดเผยว่า  
+                                กนอ.มีแนวคิดจัดระเบียบสายสื่อสารในนิคมอุตสาหกรรมลงใต้ดิน เพื่อเพิ่มความ
+                                ปลอดภัยต่อชีวิตและทรัพย์สิน รวมทั้งยังเป็นการเฝ้าระวังอุบัติเหตุที่อาจจะเกิดขึ้น 
+                                และจัดเตรียมระบบสาธารณูปโภค สิ่งอำนวยความสะดวกและการบริการให้กับผู้ประกอบ
+                                การในนิคมอุตสาหกรรม เพื่อให้สามารถใช้ประโยชน์ร่วมกันได้
+                            </p>
+                            <ol>
+                                <li>ปลอดภัยต่อชีวิตและทรัพย์สิน รวมทั้งยังเป็นการเฝ้าระวังอุบัติเหตุที่อาจจะเกิดขึ้น</li>
+                                <li>ปลอดภัยต่อชีวิตและทรัพย์สิน รวมทั้งยังเป็นการเฝ้าระวังอุบัติเหตุที่อาจจะเกิดขึ้น</li>
+                                <li>ปลอดภัยต่อชีวิตและทรัพย์สิน รวมทั้งยังเป็นการเฝ้าระวังอุบัติเหตุที่อาจจะเกิดขึ้น</li>
+                            </ol>
+
+                            <h3 class="xs fw-500 color-06 xs-no-br mt-4 pt-2">
+                                นอกจากนี้ ยังเป็นการรองรับการขยายตัว <br>
+                                ของอุตสาหกรรมโทรคมนาคมที่สูงขึ้นในอนาคต 
+                            </h3>
+                            <p class="mt-3">
+                                โดยล่าสุดในการประชุมคณะกรรมการ กนอ.(บอร์ด กนอ.) ที่มีนายนรินทร์ กัลยาณมิตร ประธานบอร์ด 
+                                กนอ.เป็นประธานการประชุม มีมติเห็นชอบในหลักการของแนวทาง
+                                ดังกล่าวตามที่ กนอ.เสนอ โดยให้เริ่มศึกษาความเป็นไปได้ของการลงทุนฯ ในปี 2564 
+                                และคาดว่าหากผลการศึกษาแล้วเสร็จจะสามารถเริ่มดำเนินโครงการวางท่อร้อยสาย
+                                สื่อสารใต้ดินในนิคมอุตสาหกรรมได้ในปี 2567 โดยจะเป็นการร่วมทุนระหว่างรัฐและเอกชน (PPP) 
+                                หรือ พีพีพ
+                            </p>
+                            <ul>
+                                <li>ปลอดภัยต่อชีวิตและทรัพย์สิน รวมทั้งยังเป็นการเฝ้าระวังอุบัติเหตุที่อาจจะเกิดขึ้น</li>
+                                <li>ปลอดภัยต่อชีวิตและทรัพย์สิน รวมทั้งยังเป็นการเฝ้าระวังอุบัติเหตุที่อาจจะเกิดขึ้น</li>
+                                <li>ปลอดภัยต่อชีวิตและทรัพย์สิน รวมทั้งยังเป็นการเฝ้าระวังอุบัติเหตุที่อาจจะเกิดขึ้น</li>
+                            </ul>
+
+                            <div class="pt-3">
+                                <p class="mt-3">
+                                    <img class="content-img-float left" src="public/assets/app/images/bg/18.jpg" alt="Image" />
+                                    โดยอุตสาหกรรมดิจิทัล เป็นหนึ่งในกลุ่มอุตสาหกรรมที่ใช้เทคโนโลยีขั้นสูง และเป็นอุตสาหกรรมเป้าหมาย
+                                    ของการพัฒนาในพื้นที่อีอีซี ซึ่งนักลงทุนต่างชาติโดยเฉพาะนักลงทุนจีนได้ย้ายฐานการผลิตมายังภูมิภาค
+                                    อาเซียนจนยอดการลงทุนโดยตรงจากจากต่างประเทศ (Foreign Direct Investment : FDI) เพิ่มมากขึ้น
+                                    ถึงแม้ว่าในช่วงไตรมาส 2 จะชะลอตัวลงเล็กน้อยจากสถานการณ์โควิด-19 แต่หลังจากที่รัฐบาลได้ประกาศ
+                                    ผ่อนปรนมาตรการต่างๆ ทำให้บรรยากาศการลงทุนเริ่มกลับมาคึกคักอีกครั้ง
+                                </p>
+                                <h4 class="xs-no-br mt-4">
+                                    โดยเฉพาะในกลุ่มอุตสาหกรรม <br>
+                                    เป้าหมายเช่น อุตสาหกรรมดิจิทัล เพื่อพัฒนาระบบ 5G <br>
+                                    และนวัตกรรมด้านดิจิทัลสมัยใหม่
+                                </h4>
+                            </div>
+                        </div>
+                        
+                        <div class="clear-both"></div>
+                        <div class="mt-4" data-aos="fade-up" data-aos-delay="0">
+                            <?php include('component/content-footer.php'); ?>
+                        </div>
+                        <div class="btns text-center" data-aos="fade-up" data-aos-delay="0">
+                            <a class="btn btn-action" href="#">
+                                <span>บทความต่อไป</span>
+                            </a>
+                        </div>
+                        
+                        <div class="mt-4 pt-4" data-aos="fade-up" data-aos-delay="0">
+                            <h3 class="xs content-grid-title">
+                                บทความที่เกี่ยวข้อง
+                            </h3>
+                            <div class="grids">
+                                <?php for($j=0; $j<3; $j++){?>
+                                    <div class="grid md-1-3 sm-50">
+                                        <div class="ss-card ss-card-01">
+                                            <a class="ss-img" href="#">
+                                                <div class="img-bg lazy-bg" data-src="public/assets/app/images/bg/17.jpg"></div>
+                                                <div class="hover-container">
+                                                    <div class="icon">
+                                                        <img src="public/assets/app/images/icon/06.png" alt="Icon" />
+                                                    </div>
+                                                </div>
+                                                <div class="date">
+                                                    <div class="d">04</div>
+                                                    <div class="m-y">09.2563</div>
+                                                </div>
+                                            </a>
+                                            <div class="text-container">
+                                                <div class="ss-box-shadow"></div>
+                                                <div class="text-wrapper">
+                                                    <a class="title h6 fw-600" href="#">
+                                                        กนอ.ดึงเอกชนร่วมพีพีพีจัดระเบียบ
+                                                        สายสื่อสารลงดิน วางระบบ 5G
+                                                    </a>
+                                                    <p class="desc mt-1">
+                                                        บอร์ดกนอ.ไฟเขียวลงทุนนำสายสื่อสารในนิคมอุตสาหกรรมลงใต้ดิน 
+                                                        รองรับ Smart City เตรียมวางระบบ 5 G ครอบคลุมพื้นที่นิคมอุตสาห
+                                                        กรรรม 14 แห่ง และท่าเรืออุตสาหกรรม 1 แห่ง คาดเริ่มได้ปี’67  
+                                                    </p>
+                                                    <div class="ss-stat">
+                                                        <div class="stat">
+                                                            <div class="icon">
+                                                                <img src="public/assets/app/images/icon/07.png" alt="Icon" />
+                                                            </div> 112
+                                                        </div>
+                                                        <div class="stat">
+                                                            <div class="icon">
+                                                                <i class="zmdi zmdi-share"></i>
+                                                            </div> 20
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php }?> 
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
 
     <?php include_once('include/footer-inside.php'); ?>
     <?php include_once('include/script.php'); ?>
