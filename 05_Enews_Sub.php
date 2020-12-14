7274541a7def9fd87bbf93063c886ab21a8a612c
Tonnpo <tonn_po@hotmail.com>
2020-12-14 23:37:56 +0700

    update 14/12

--- 05_Enews_Sub.php
+++ 05_Enews_Sub.php
@@ -3,11 +3,176 @@
 <head>
     <?php include_once('include/header.php'); ?>
     <?php include_once('include/style.php'); ?>
+
+    <style>
+        /* Fields & Inputs */
+        .field{margin:.75rem 0 0 0; width:100%;}
+        .field label{font-size:1.375rem; font-weight:200; margin:0;}
+
+        .field > .control, .field > .control-flex{position:relative; width:100%; margin:.25rem 0 0 0;}
+        .field > .control:first-child{margin:0;}
+        .field > .control-flex{display:flex; align-items:center; flex-wrap:wrap; margin:.5rem 0 0 0;}
+        .field > .control-flex > *{
+            margin-right:.75rem; margin-top:.25rem; margin-bottom:.25rem; width:11rem;
+        }
+
+        .field input[type=text], .field input[type=email], .field input[type=password], 
+        .field input[type=date], .field input[type=tel], .field input[type=phone], 
+        .field input[type=number], .field select, .field textarea, 
+        .field > .control.file-control > .file-upload{
+            width:100%; border:1px solid transparent; padding:.4375rem 1rem; margin:0; background:#e7e7e7;
+            font-size:1.25rem; color:#000; font-weight:300; border-radius:.1875rem;
+            -webkit-box-shadow:inset 0px 0px 3px 1px rgba(0,0,0,.05);
+            -moz-box-shadow:inset 0px 0px 3px 1px rgba(0,0,0,.05);
+            box-shadow:inset 0px 0px 3px 1px rgba(0,0,0,.05);
+            transition:border-color .3s;
+        }
+        .field input[type=text]:focus, .field input[type=email]:focus, .field input[type=password]:focus, 
+        .field input[type=date]:focus, .field input[type=tel]:focus, .field input[type=phone]:focus, 
+        .field input[type=number]:focus, .field select:focus, .field textarea:focus{
+            border-color:#0d838d; border-color:var(--color4);
+        }
+
+        .field .control.checkbox-control, .field .control.radio-control{display:flex; position:relative;}
+        .field .control.radio-control > input[type=radio]{position:absolute; opacity:0;}
+        .field .control.checkbox-control > input[type=checkbox]{
+            position:absolute; margin:0; font-size:.875rem; width:.875rem; height:.875rem;
+            cursor:pointer; top:.25rem;
+        }
+        .field .control.checkbox-control > label, .field .control.radio-control > label{
+            width:100%; padding:0 0 0 1.5rem; cursor:pointer;
+        }
+
+        .btn-cancel, .btn-cancel:focus, .btn-cancel:hover, .btn-cancel:active{
+            display:inline-block; position:relative; border:0; border-radius:0;
+            font-size:1.375rem; font-weight:300; padding:.25rem 2.5rem .25rem 1.5rem; overflow:hidden;
+            white-space:nowrap; min-width:9.75rem; text-align:center;
+        }
+        /* Color-01 */
+        .btn-cancel { background: #3d3d3d; color: #fff;}
+        .btn-cancel:focus, .btn-cancel:hover, .btn-cancel:active{ color:#fff;}
+        .btn-cancel::before{ background: #ccc; }
+        .btn-cancel::before{
+            content:''; position:absolute; top:0; bottom:0; right:0; width:1.75rem;
+            clip-path:polygon(0 0, 100% 0, 100% 100%, 0 100%, 40% 50%); transition:transform .3s;
+        }
+
+        .btn-cancel:hover::before{transform:translateX(calc(1.75rem * .6));}
+    </style>
 </head>
 <body class="loading">
     <?php include_once('include/topnav.php'); ?>
 
-    <img class="img" src="public/assets/app/images/page/05_Enews_Sub.jpg" alt="Page" />
+    <?php
+        $breadcrumb = [
+            [ 'url' => '#', 'display' => 'กนอ.อัพเดท' ],
+            [ 'url' => '#', 'display' => 'แจ้งเรื่องร้องเรียน' ],
+        ];
+        include('component/breadcrumb.php');
+    ?>
+
+
+    <section class="section-bpadding section-bg-hero">
+        <div class="bg-hero lazy-bg" data-src="public/assets/app/images/hero/10.jpg"></div>
+        <div class="container">
+            <h2 data-aos="fade-up" data-aos-delay="0">
+                จดหมายข่าวสาร
+            </h2>
+            <h3 class="sm color-02 fw-400" data-aos="fade-up" data-aos-delay="150">
+                สมัครรับจดหมายข่าวผ่านทางอีเมล์
+            </h3>
+            <h5 class="xs-no-br" data-aos="fade-up" data-aos-delay="300">
+                จัดการเนื้อหาในเว็บไซต์และข้อมูลส่วนตัว
+            </h5>
+
+            <div data-aos="fade-up" data-aos-delay="450">
+                <?php include('component/grid-header.php'); ?>
+            </div>
+
+            <div class="tab-container tab-container-01">
+                <div class="grids">
+
+                    <div class="grid xl-20 lg-25 sm-100" data-aos="fade-up" data-aos-delay="600">
+                        <?php
+                            $cateActive = 0;
+                            $cateChildren = false;
+                            include('component/category-tab.php');
+                        ?>
+                    </div>
+
+                    <div class="grid xl-80 lg-75 sm-100 mt-0" data-aos="fade-up" data-aos-delay="750">
+                        <div class="grids">
+                            <div class="grid sm-100">
+
+                                <div class="bg-01 px-4 py-3">
+                                    <h5 class="fw-200 color-white">แบบฟอร์มสมัครรับข่าวสาร</h3>
+                                </div>
+
+                                <div class="bg-white px-4 pt-3 pb-4 box-shadow">
+
+                                    <form action="./" method="POST">   
+                                        <div class="field">
+                                            <div class="control">
+                                                <input type="text" placeholder="กรุณากรอกอีเมลที่ต้องการรับข่าวสาร *" style="width:80%;">
+                                            </div>
+                                        </div> 
+
+
+
+
+                                        <h5 class="fw-400 mt-3">กรุณาเลือกประเภทของข่าวสารที่คุณมีความสนใจในการติดตาม</h5>
+                                        <div class="border-bottom pt-3 pb-3" style="border-color: #dee2e6!important;">
+                                            <h6 class="color-01 fw-400">ข่าวประชาสัมพันธ์</h6>
+                                            <p class="fw-200">ด้วยการติดตามกิจกรรมข่าวสาร และข้อมูลอัพเดทต่างๆเกี่ยวกับ กนอ.และนิคมอุตสาหกรรมในประเทศไทย</p>
+                                            <?php for($i=0; $i<6;$i++){?>
+                                                <div class="field mt-3">
+                                                    <div class="control checkbox-control">
+                                                        <input type="checkbox" name="types[]" id="type_1_<?= $i; ?>"  value="<?= $i; ?>">
+                                                        <label class="mb-0 " for="type_1_<?= $i; ?>">ประกาศจาก กนอ</label>
+                                                    </div>
+                                                </div>
+                                            <?php }?>
+                                        </div>
+
+                                        <div class="border-bottom pt-3 pb-3" style="border-color: #dee2e6!important;">
+                                            <h6 class="color-01 fw-400">สื่อมัลติมีเดียและคลังความรู้</h6>
+                                            <p class="fw-200">เพื่อยกระดับและพัฒนาศักยภาพสำหรับกิจการในนิคมอุตสาหกรรม</p>
+                                            <?php for($i=0; $i<3;$i++){?>
+                                                <div class="field mt-3">
+                                                    <div class="control checkbox-control">
+                                                        <input type="checkbox" name="types[]" id="type_2_<?= $i; ?>"  value="<?= $i; ?>">
+                                                        <label class="mb-0 " for="type_2_<?= $i; ?>">คลังภาพ</label>
+                                                    </div>
+                                                </div>
+                                            <?php }?>
+                                        </div>
+                                            
+
+                                        <img class="mt-4" src="./public/assets/app/images/icon/capcha.png" alt="capcha">
+                                        
+                                        <div class="btns">
+                                            <a href="#" class="btn btn-action mr-1">สมัครรับข่าวสาร</a>
+                                            <a href="#" class="btn btn-cancel">ข้อมูล</a>
+                                        </div>
+
+
+                                        <h6 class="mt-4">เงื่อนไขการรับจดหมายข่าว</h6>
+                                        <p class="fw-200">เมื่อส่งแบบฟอร์มนี้ จะถือว่าคุณยินยอมที่จะรับการติดต่อสื่อสารทางอิเล็กทรอนิกส์จากกรมการนิคมอุตสาหกรรมแห่งประเทศไทย</p>
+                                    </form>
+
+                                </div>
+
+
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
 
     <?php include_once('include/footer-inside.php'); ?>
     <?php include_once('include/script.php'); ?>
--- 07_KM.php
+++ 07_KM.php
@@ -3,12 +3,180 @@
 <head>
     <?php include_once('include/header.php'); ?>
     <?php include_once('include/style.php'); ?>
+
+
+    <style>
+        /* Custom stytle for ss-card-03 */
+        @media screen and (min-width:768px) {
+            .ss-card-03.ss-card-03-custom{padding: 0 55% 0 0;}
+            .ss-card-03.ss-card-03-custom .text-container {
+                width:65%;
+            }
+            .ss-card-03.ss-card-03-custom .text-container .text-wrapper a.title.h4 {font-size:1.5625rem;}
+            .ss-card-03.ss-card-03-custom .text-container .text-wrapper p.desc {font-size:110%;}
+        }
+    </style>
 </head>
 <body class="loading">
     <?php include_once('include/topnav.php'); ?>
 
-    <img class="img" src="public/assets/app/images/page/07_KM.jpg" alt="Page" />
+        <?php
+            $breadcrumb = [
+                [ 'url' => '#', 'display' => 'คลังความรู้' ],
+            ];
+            include('component/breadcrumb.php');
+        ?>
 
+        <section class="section-bpadding section-bg-hero">
+            <div class="bg-hero lazy-bg" data-src="public/assets/app/images/hero/07.jpg"></div>
+            <div class="container">
+                <h2 class="color-white" data-aos="fade-up" data-aos-delay="0">
+                    คลังความรู้
+                </h2>
+                <h3 class="sm color-white fw-400" data-aos="fade-up" data-aos-delay="150">
+                    องค์ความรู้ในด้านต่าง ๆ เกี่ยวกับอุตสาหากรรม
+                </h3>
+                <h5 class="color-white xs-no-br" data-aos="fade-up" data-aos-delay="300">
+                    เพื่อยกระดับและพัฒนาศักยภาพสำหรับกิจกรรมในนิคมอุตสาหกรรม <br>
+                    กนอ. ได้รับว่าเป็นเครื่องการันตีถึงการดำเนินการของบริษัทที่ดีและมีการบูรณาการ <br>
+                    อีกทั้งพัฒนาอย่างต่อเนื่อง
+                </h5>
+
+                <div data-aos="fade-up" data-aos-delay="450">
+                    <?php include('component/grid-header.php'); ?>
+                </div>
+
+                <div class="tab-container tab-container-01">
+                    <div class="grids">
+
+                        <div class="grid xl-20 lg-25 sm-100" data-aos="fade-up" data-aos-delay="600">
+                            <?php
+                                $cateActive = 1;
+                                $cateChildren = false;
+                                include('component/category-tab.php');
+                            ?>
+                        </div>
+
+                        <div class="grid xl-80 lg-75 sm-100 mt-0" data-aos="fade-up" data-aos-delay="750">
+                            <div class="grids">
+                                <div class="grid sm-100">
+
+                                    <div class="ss-card ss-card-03">
+                                        <a class="ss-img" href="#">
+                                            <div class="img-bg lazy-bg" data-src="public/assets/app/images/bg/17.jpg"></div>
+                                            <div class="hover-container">
+                                                <div class="icon">
+                                                    <img src="public/assets/app/images/icon/06.png" alt="Icon" />
+                                                </div>
+                                            </div>
+                                            <div class="date bg-05">
+                                                <div class="d">04</div>
+                                                <div class="m-y">09.2563</div>
+                                            </div>
+                                            <div class="tag-star">
+                                                <img src="public/assets/app/images/icon/star.png" alt="Icon" />
+                                            </div>
+                                        </a>
+                                        <div class="text-container">
+                                            <div class="wrapper">
+                                                <div class="ss-box-shadow fade-right"></div>
+                                                <div class="text-wrapper">
+                                                    <a class="title h4" href="#">
+                                                        กนอ.ดึงเอกชนร่วมพีพีพีจัดระเบียบ
+                                                        สายสื่อสารลงดิน วางระบบ 5G
+                                                    </a>
+                                                    <p class="desc mt-1">
+                                                        บอร์ดกนอ.ไฟเขียวลงทุนนำสายสื่อสารในนิคมอุตสาหกรรมลงใต้ดิน 
+                                                        รองรับ Smart City เตรียมวางระบบ 5 G ครอบคลุมพื้นที่นิคมอุตสาห
+                                                        กรรรม 14 แห่ง และท่าเรืออุตสาหกรรม 1 แห่ง คาดเริ่มได้ปี’67  
+                                                    </p>
+                                                    <div class="ss-stat">
+                                                        <div class="stat">
+                                                            <div class="icon">
+                                                                <img src="public/assets/app/images/icon/07.png" alt="Icon" />
+                                                            </div> 112
+                                                        </div>
+                                                        <div class="stat">
+                                                            <div class="icon">
+                                                                <i class="zmdi zmdi-share"></i>
+                                                            </div> 20
+                                                        </div>
+                                                    </div>
+                                                    <div class="ss-tag">
+                                                        <div class="text">TAG :</div> 
+                                                        <div class="tags">
+                                                            <a class="tag" href="#">นิคมอุตสาหกรรม</a>
+                                                            <a class="tag" href="#">กนอ.</a>
+                                                            <a class="tag" href="#">5G</a>
+                                                            <a class="tag" href="#">สายสื่อสาร</a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <div class="grid sm-100 mt-0">
+                                    <div class="grids">
+
+                                        <?php for($j=0; $j<9; $j++){?>
+                                            <div class="grid sm-100">
+                                                <div class="ss-card ss-card-03 ss-card-03-custom">
+                                                    <a class="ss-img" href="#">
+                                                        <div class="img-bg lazy-bg" data-src="public/assets/app/images/bg/17.jpg"></div>
+                                                        <div class="hover-container">
+                                                            <div class="icon">
+                                                                <img src="public/assets/app/images/icon/06.png" alt="Icon" />
+                                                            </div>
+                                                        </div>
+                                                        <div class="date bg-05">
+                                                            <div class="d">04</div>
+                                                            <div class="m-y">09.2563</div>
+                                                        </div>
+                                                    </a>
+                                                    <div class="text-container">
+                                                        <div class="wrapper">
+                                                            <div class="ss-box-shadow fade-right"></div>
+                                                            <div class="text-wrapper">
+                                                                <a class="title h4" href="#">
+                                                                    กนอ.ดึงเอกชนร่วมพีพีพีจัดระเบียบ
+                                                                    สายสื่อสารลงดิน วางระบบ 5G
+                                                                </a>
+                                                                <p class="desc mt-1">
+                                                                    บอร์ดกนอ.ไฟเขียวลงทุนนำสายสื่อสารในนิคมอุตสาหกรรมลงใต้ดิน 
+                                                                    รองรับ Smart City เตรียมวางระบบ 5 G ครอบคลุมพื้นที่นิคมอุตสาห
+                                                                    กรรรม 14 แห่ง และท่าเรืออุตสาหกรรม 1 แห่ง คาดเริ่มได้ปี’67  
+                                                                </p>
+                                                                <div class="ss-stat">
+                                                                    <div class="stat">
+                                                                        <div class="icon">
+                                                                            <img src="public/assets/app/images/icon/07.png" alt="Icon" />
+                                                                        </div> 112
+                                                                    </div>
+                                                                    <div class="stat">
+                                                                        <div class="icon">
+                                                                            <i class="zmdi zmdi-share"></i>
+                                                                        </div> 20
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        <?php }?> 
+
+                                    </div>
+                                </div>
+                            </div>
+                            <?php include('component/grid-footer.php'); ?>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </section>
     <?php include_once('include/footer-inside.php'); ?>
     <?php include_once('include/script.php'); ?>
 </body>
