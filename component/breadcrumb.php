02fcfe36b7848937d8951556505a2368cf2d89c9
Sarun Seepun <sarun_sla@hotmail.com>
2020-12-20 07:10:22 -0800

    W3C updates

--- component/breadcrumb.php
+++ component/breadcrumb.php
@@ -1,19 +1,16 @@
 <?php if(!empty($breadcrumb)){?>
     <section class="breadcrumb-01 <?php if(!empty($breadcrumbClass))echo $breadcrumbClass; ?>">
         <div class="container">
+            <h1 class="d-none">Breadcrumb 01</h1>
             <div class="wrapper">
-                <h6>
-                    <a class="icon" href="#">
-                        <em class="zmdi zmdi-home"></em>
-                    </a>
-                </h6>
+                <a class="icon" href="#">
+                    <em class="zmdi zmdi-home"></em>
+                </a>
                 <?php foreach($breadcrumb as $b){?>
-                    <h6>
-                        <a class="text" href="<?= $b['url'] ?>">
-                            <em class="zmdi zmdi-chevron-right"></em> 
-                            <?= $b['display'] ?>
-                        </a>
-                    </h6>
+                    <a class="text" href="<?= $b['url'] ?>">
+                        <em class="zmdi zmdi-chevron-right"></em> 
+                        <?= $b['display'] ?>
+                    </a>
                 <?php }?>
             </div>
         </div>
