e682a46239ea389f5484c819a8f16f60bab64279
sseepun <sarun_sla@hotmail.com>
2021-02-07 18:23:10 +0700

    Update calendar page

--- public/assets/app/js/script.js
+++ public/assets/app/js/script.js
@@ -572,41 +572,39 @@ $(function(){ 'use strict';
     if(tabContainers.length){
         tabContainers.each(function(){
             var self = $(this);
-            var tabs = self.find('.tabs .tab'),
-                tabChildren = self.find('.tabs .tab-children'),
-                tabContents = self.find('.tab-contents .tab-content');
-            if(self.hasClass('tab-global')){
-                var temp = $('.tab-container.tab-global-target');
-                tabContents = temp.find('.tab-contents .tab-content');
-            }
-            tabs.click(function(e){
-                var temp = $(this),
-                    tabId = temp.data('tab');
-                if(tabId!==undefined){
-                    var target = tabContents.filter('[data-tab="'+tabId+'"]');
-                    if(target.length){
-                        e.preventDefault();
+            if(!self.hasClass('tab-local') && !self.hasClass('tab-local-target')){
+                var tabs = self.find('.tabs .tab'),
+                    tabChildren = self.find('.tabs .tab-children'),
+                    tabContents = self.find('.tab-contents .tab-content');
+                tabs.click(function(e){
+                    var temp = $(this),
+                        tabId = temp.data('tab');
+                    if(tabId!==undefined){
+                        var target = tabContents.filter('[data-tab="'+tabId+'"]');
+                        if(target.length){
+                            e.preventDefault();
 
-                        tabChildren.stop().slideUp();
-                        var children = temp.next();
-                        if(children.hasClass('tab-children')){
-                            children.stop().slideDown();
+                            tabChildren.stop().slideUp();
+                            var children = temp.next();
+                            if(children.hasClass('tab-children')){
+                                children.stop().slideDown();
+                            }
+
+                            tabs.removeClass('active');
+                            temp.addClass('active');
+                            tabContents.removeClass('active');
+                            target.addClass('active');
+
+                            var slides = target.find('.slide-container > .slides');
+                            if(slides.length){
+                                slides.slick('setPosition');
+                            }
+
+                            AOS.refresh();
                         }
-
-                        tabs.removeClass('active');
-                        temp.addClass('active');
-                        tabContents.removeClass('active');
-                        target.addClass('active');
-
-                        var slides = target.find('.slide-container > .slides');
-                        if(slides.length){
-                            slides.slick('setPosition');
-                        }
-
-                        AOS.refresh();
                     }
-                }
-            });
+                });
+            }
         });
     }
 
