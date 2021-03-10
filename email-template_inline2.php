b12781e50c45130238c8c1b982577877143b0402
name_ati <name_with@hotmail.com>
2021-03-10 10:09:49 +0700

    [Add] Html_EmailTemplate#7

--- email-template_inline2.php
+++ email-template_inline2.php
@@ -3,7 +3,7 @@
 <head>
 <link rel="stylesheet" href="http://ieat-web-html.jigsawgroups.work/public/assets/app/fonts/includes.css">
 <link rel="stylesheet" href="http://ieat-web-html.jigsawgroups.work/public/assets/lib/material-design-iconic-font-2.2.0/css/material-design-iconic-font.min.css">
-<link rel="stylesheet" href= "http://ieat-web-html.jigsawgroups.work/public/assets/app/css/style.css">
+<link rel="stylesheet" href="http://ieat-web-html.jigsawgroups.work/public/assets/app/css/style.css">
 <link rel="stylesheet" href="http://ieat-web-html.jigsawgroups.work/public/assets/app/css/color-0.css">
 </head>
 <body style="max-width:768px;margin:auto;overflow-x: hidden;overflow-y: auto;font-size: 16px;font-weight: 300;line-height: 1.1;font-family: 'DB HEAVENT', sans-serif;">
@@ -42,8 +42,8 @@
                 <h4 style="text-align:center;margin-top:10rem;margin-bottom:10rem;">[Content section]</h4>      
                 </div>
             </nav>
-            <div style="background-image: linear-gradient(rgba(255,255,255,1), transparent, transparent), url('public/assets/app/images/banner/01.jpg');text-align:center;background-position: -150px 90px;background-size: 190%; background-repeat: no-repeat;">
-                <h5 style="font-weight:500; color:#4A2E6D;padding-top:2rem;">คุณสามารถยกเลิกอีเมล์ข่าวสาร โดยคลิกที่ปุ่มด้านล่าง</h5>
+            <div style="background-image: linear-gradient(rgba(255,255,255,1), transparent, transparent), url('public/assets/app/images/banner/01.jpg');text-align:center;background-position: -310px 130px;background-size: 250%; background-repeat: no-repeat;">
+                <h5 style="font-weight:500; color:#4A2E6D;padding-top:2rem;">คุณสามารถยกเลิกอีเมลข่าวสาร โดยคลิกที่ปุ่มด้านล่าง</h5>
                 <div>
                 <button class="btn-action"style="margin-top:1rem;background-color:#3D3D3D;font-weight: 400;border: 1px solid transparent;padding: .375rem .75rem;
                 font-size: 1.375rem;line-height: 1.5;transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;">
@@ -57,25 +57,24 @@
                 <div>
                     <div style="display:block;">
                             <div style="text-align:center;padding: 25px 0px 25px 0px;font-size:1.5rem;">
-                                <span style="font-size:1.8rem;">การนิคมอุตสาหกรรมแห่งประเทศไทย (กนอ.)</span><br>
-                                <span> 618 ถนนนิคมมักกะสัน แขวงมักกะสัน เขตราชเทวี กรุงเทพมหานคร 10400</span><br>
-                                <span style="color:#6cdded; margin-left:.5rem;">โทรศัพท์</span> 
+                                <span style="font-size:1.8rem;font-weight:300;">การนิคมอุตสาหกรรมแห่งประเทศไทย (กนอ.)</span><br>
+                                <span style="font-weight:200;"> 618 ถนนนิคมมักกะสัน แขวงมักกะสัน เขตราชเทวี กรุงเทพมหานคร 10400</span><br>
+                                <span style="color:#6cdded; margin-left:.5rem;font-weight:200;margin-right:0.3rem;">โทรศัพท์.</span> 
                                 <a id="blue-h"style="font-weight:500;color:white;margin-right:0.5rem;" href="#">
                                     02 2530561
                                 </a> 
-                                <span style="color:#6cdded; margin-left:.5rem;">โทรสาร</span> 
+                                <span style="color:#6cdded; margin-left:.5rem;font-weight:200;margin-right:0.3rem;">โทรสาร.</span> 
                                 <a style="font-weight:500;color:white;margin-right:0.5rem;" href="#">
                                     02 2526582 , 02 2534086 </a>
-                                <span style="color:#6cdded; margin-left:.5rem;">อีเมล์</span>
+                                <span style="color:#6cdded; margin-left:.5rem;font-weight:200;margin-right:0.3rem;">อีเมล</span>
                                 <a style="font-weight:500;color:white;margin-right:0.5rem;" href="#">
                                     contact@ieat.mail.go.th
                                 </a>
-                                <br>				
-                                <span style="font-size:80%">
-                                    © สงวนสิทธ์ 2564 การนิคมอุตสาหกรรมแห่งประเทศไทย (กนอ.)
-                                </span>
+                                <br>
+                                <span>
+                                    <p style="margin-top:1rem;font-weight:200;">© สงวนสิทธ์ 2564 การนิคมอุตสาหกรรมแห่งประเทศไทย (กนอ.) </p>
+                                </span>				
                             </div>
-                         
                     </div>
                 </div>
             </nav>
