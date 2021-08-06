c95e0b55006c74341102a7a455687546ad35b4a9
sseepun <sarun_sla@hotmail.com>
2021-08-07 02:59:45 +0700

    Complete minisite intro pages

---
+++ minisite-00-intro-02.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="th">
+<head>
+    <?php include_once('include/header.php'); ?>
+    <?php include_once('include/style.php'); ?>
+    <link rel="stylesheet" href="public/assets/app/css/style-minisite.css" />
+</head>
+<body class="loading">
+    <section class="intro-01">
+        <div class="content-container white-tablet" style="background-image:url('public/assets/app/images/bg/intro-02.jpg');">
+            <div class="wrapper" data-aos="fade-up" data-aos-delay="150">
+                <img class="hero-text-02" src="public/assets/app/images/hero/intro-02-text.png" alt="Image Text" />
+            </div>
+            <div class="cover-2"></div>
+            <div class="cover-1"></div>
+        </div>
+        <img class="pattern" src="public/assets/app/images/hero/02.png" alt="Image Pattern" />
+        <a class="logo" href="#">
+            <img src="public/assets/app/images/logo-white.png" alt="Logo" />
+            <div class="text-wrapper color-black">
+                <h6 class="h5 fw-600 lh-sm">การนิคมอุตสาหกรรมแห่งประเทศไทย (กนอ.)</h6>
+                <p class="fw-400">Industrial Estate Authority of Thailand</p>
+            </div>
+        </a>
+    </section>
+    <?php include_once('include/script.php'); ?>
+</body>
+</html>
